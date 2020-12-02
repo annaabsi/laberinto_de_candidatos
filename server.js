025e0abe9ea687c96d8dcdcd2ae1889196e00ba0
Anna Absi <anna.absi@gmail.com>
2020-12-01 20:24:12 -0500

    server.js route index.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var PORT = process.env.PORT || 3000;
-var FACTORIAL = path.join(__dirname, 'build/factorial.min.js');
+var FACTORIAL = path.join(__dirname, 'index.html');
 
 express()
   .use(hello)
