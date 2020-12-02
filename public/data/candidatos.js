122034992668f5bcc50a69e4527de2b94d387fb3
Anna Absi <anna.absi@gmail.com>
2020-12-01 23:55:40 -0500

    candidatos.js modif

--- public/data/candidatos.js
+++ public/data/candidatos.js
@@ -1,6 +1,6 @@
 var canDatos = [
 
-    candidato3 = {
+    candidato1 = {
         nombre:"Yonhy Lescano",
         partido:"Acción Popular",
         twitter:"@",
@@ -12,7 +12,7 @@ var canDatos = [
         bio:"Lorem Ipsum"
     },
 
-    candidato4 = {
+    candidato2 = {
         nombre:"Nidia Vilchez",
         partido:"APRA",
         twitter:"@",
@@ -24,7 +24,7 @@ var canDatos = [
         bio:"Lorem Ipsum"
     },
 
-    candidato7 = {
+    candidato3 = {
         nombre:"Fernando Cillóniz",
         partido:"Todos por el Perú",
         twitter:"@",
@@ -36,7 +36,7 @@ var canDatos = [
         bio:"Lorem Ipsum"
     },
 
-    candidato10 = {
+    candidato4 = {
         nombre:"Ciro Gálvez Herrera",
         partido:"Renacimiento Andino",
         twitter:"@",
@@ -48,7 +48,7 @@ var canDatos = [
         bio:"Lorem Ipsum"
     },
 
-    candidato12 = {
+    candidato5 = {
         nombre:"Julio Guzmán",
         partido:"Partido Morado",
         twitter:"@",
@@ -59,5 +59,196 @@ var canDatos = [
         link:"danielscioli.com.ar",
         bio:"Lorem Ipsum"
     },
-    
+
+    candidato6 = {
+        nombre:"José Vega",
+        partido:"Unión por el Perú",
+        twitter:"@",
+        foto:"img/cand-gr/vega.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato7 = {
+        nombre:"Alberto Beingolea",
+        partido:"",
+        twitter:"@",
+        foto:"img/cand-gr/beingolea.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato8 = {
+        nombre:"Andrés Alcántara",
+        partido:"PPC",
+        twitter:"@",
+        foto:"img/cand-gr/alcantara.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato9 = {
+        nombre:"César Acuña",
+        partido:"Alianza para el Progreso",
+        twitter:"@",
+        foto:"img/cand-gr/acuña.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato10 = {
+        nombre:"Daniel Salaverry",
+        partido:"Somos Perú",
+        twitter:"@",
+        foto:"img/cand-gr/salaverry.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato11 = {
+        nombre:"Daniel Urresti",
+        partido:"Podemos Perú",
+        twitter:"@",
+        foto:"img/cand-gr/urresti.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato12 = {
+        nombre:"Francisco Diez Canseco",
+        partido:"Perú Nación",
+        twitter:"@",
+        foto:"img/cand-gr/diezcanseco.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato13 = {
+        nombre:"George Forsyth",
+        partido:"Restauración Nacional",
+        twitter:"@",
+        foto:"img/cand-gr/forsyth.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato14 = {
+        nombre:"Hernando de Soto",
+        partido:"Avanza País",
+        twitter:"@",
+        foto:"img/cand-gr/desoto.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato15 = {
+        nombre:"Keiko Fujimori",
+        partido:"Fuerza Popular",
+        twitter:"@",
+        foto:"img/cand-gr/fujimori.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato16 = {
+        nombre:"Marco Arana",
+        partido:"Frente Amplio",
+        twitter:"@",
+        foto:"img/cand-gr/arana.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato17 = {
+        nombre:"Ollanta Humala",
+        partido:"Partido Nacionalista",
+        twitter:"@",
+        foto:"img/cand-gr/humala.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato18 = {
+        nombre:"Pedro Castillo",
+        partido:"Perú Libre",
+        twitter:"@",
+        foto:"img/cand-gr/castillo.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato19 = {
+        nombre:"Rafael López Aliaga",
+        partido:"Solidaridad Nacional",
+        twitter:"@",
+        foto:"img/cand-gr/lopezaliaga.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+    candidato20 = {
+        nombre:"Rafael Santos",
+        partido:"Perú Patria Segura",
+        twitter:"@",
+        foto:"img/cand-gr/santos.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
+
+     candidato21 = {
+        nombre:"Verónika Mendoza",
+        partido:"Juntos por el Perú",
+        twitter:"@",
+        foto:"img/cand-gr/mendoza.png",
+        edad:"XX Años",
+        profesion:"Lorem Ipsum",
+        web:"http://www.danielscioli.com.ar",
+        link:"danielscioli.com.ar",
+        bio:"Lorem Ipsum"
+    },
 ]
